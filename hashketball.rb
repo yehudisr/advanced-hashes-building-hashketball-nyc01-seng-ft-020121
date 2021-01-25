23b790178f3bf6a24c17fea5022ba920e6a05475
yehudis raitman <yehudisr@gmail.com>
2021-01-25 00:19:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,12 +4,12 @@ def game_hash
    :home => {
      :team_name => "",
      :colors => [],
-     :team_players => []
+     :players => []
    },
    :away => {
      :team_name => "",
      :colors => [],
-     :team_players => []
+     :players => []
    }
  }
  
