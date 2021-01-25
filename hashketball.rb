ff8d5dbbf77f4894c72e521a747d27198a6127a0
yehudis raitman <yehudisr@gmail.com>
2021-01-25 00:36:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -92,7 +92,7 @@ def game_hash
         :blocks => 5,
         :slam_dunks => 5
        },
-        {:player_name => "",
+        {:player_name => "Ben Gordon",
         :number => ,
         :shoe => ,
         :points =>,
