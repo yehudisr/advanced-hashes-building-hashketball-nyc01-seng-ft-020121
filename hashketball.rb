9633250beed78912cea49d76db2c9fdc224eea2d
yehudis raitman <yehudisr@gmail.com>
2021-01-25 00:31:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -62,15 +62,15 @@ def game_hash
      :team_name => "Charlotte Hornets",
      :colors => ["Turquoise","Purple"],
      :players => [
-        {:player_name => 
-        :number =>
-        :shoe => 
-        :points =>
-        :rebounds =>
-        :assists =>
-        :steals =>
-        :blocks =>
-        :slam_dunks => 
+        {:player_name => "Jeff Adrien",
+        :number => "4",
+        :shoe => "18",
+        :points => 10,
+        :rebounds => 1,
+        :assists => 1, 
+        :steals => 2,
+        :blocks => 7,
+        :slam_dunks => 2
        },
         {:player_name => 
         :number =>
