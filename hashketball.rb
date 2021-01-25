8ac6623d4f36740ae2593143545bdd664a5638f6
yehudis raitman <yehudisr@gmail.com>
2021-01-25 00:15:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,13 @@
-# Write your code here!
+require "pry"
+def game_hash
+ hashketball = {}
+ hashketball[:home] = {}
+ hashketball[:away] = {}
+ hashketball[:home][:team_name] = ""
+ hashketball[:home][:colors]= []
+ hashketball[:home][:players] = []
+ hashketball[:away][:team_name] = ""
+ hashketball[:away][:colors]= []
+ hashketball[:away][:players] = []
+ #binding.pry
+end  
