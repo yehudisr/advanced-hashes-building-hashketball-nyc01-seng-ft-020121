e491b3fe0dce9ba593e79282db3a64daf57ddf64
yehudis raitman <yehudisr@gmail.com>
2021-01-25 00:25:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,15 +5,15 @@ def game_hash
      :team_name => "Brooklyn Nets",
      :colors => ["Black", "White"],
      :players => [
-       {:player_name => 
-        :number =>
-        :shoe => 
-        :points =>
-        :rebounds =>
-        :assists =>
-        :steals =>
-        :blocks =>
-        :slam_dunks => 
+       {:player_name => "Alan Anderson",
+        :number => "0",
+        :shoe => "16",
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
        },
         {:player_name => 
         :number =>
